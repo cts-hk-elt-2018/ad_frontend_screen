111f0e197561843d5dbf2472024873c8f29fb9d0
ckho <me@ckho.info>
2018-11-18 01:28:36 +0800

    Add Around the World Page with animation

---
+++ src/views/AroundTheWorld.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="aroundtheworld">
+    <div>
+      <div class="lottie">
+        <lottie :options="lottieOptions" :height="350" :width="350" v-on:animCreated="handleAnimation"/>
+      </div>
+      <img class="middle-logo" src="../assets/cognizant_logo_white.png" />
+      <h1>Around the world</h1>
+    </div>
+  </div>
+</template>
+<script>
+import lottie from "vue-lottie";
+import * as animationData from "@/assets/aroundTheWorld.json";
+export default {
+  components: {
+    lottie
+  },
+  data() {
+    return {
+      lottieOptions: {
+        animationData: animationData.default
+      }
+    };
+  },
+  methods: {
+    handleAnimation: function(anim) {
+      this.anim = anim;
+    }
+  }
+};
+</script>
+<style lang="scss">
+.aroundtheworld {
+  background: #0033a0;
+  width: 100vw;
+  height: 100vh;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+  text-align: center;
+  h1 {
+    margin-top: -50px;
+    font-size: 40pt;
+    font-family: "TT Norms";
+    font-weight: 400;
+    font-style: normal;
+    color: white;
+  }
+}
+.lottie {
+  margin-top: -20px;
+}
+.middle-logo {
+  margin-top: -90px;
+  width: 60vw;
+}
+
+.inner-part {
+  // display: block;
+}
+</style>
--- src/views/LuckyDraw.vue
+++ src/views/LuckyDraw.vue
@@ -1,19 +1,86 @@
 <template>
-  <div class="about">
-    <h1>This is an lucky draw page</h1>
-    <h2>{{ giftName }}</h2>
-    <img :src="imageUrl" />
-    <div class="winner">
-      <p v-for="winner in winners" :key="winner.id" :class="winner.status">{{ winner.username }} - {{ winner.firstName }} {{ winner.lastName }}</p>
+  <div class="lucky_draw">
+    <div class="gift-photo" :style="{'order': giftId % 2 }" v-if="winners.length == 0">
+      <div class="winner-prize-image">
+        <img class="prize-img" :src="imageUrl" />
+      </div>
+    </div>
+    <div class="gift-detail" :style="{'order': (giftId + 1) % 2 }" v-if="winners.length == 0">
+      <h1 v-html="giftName"></h1>
+      <h2>{{ giftPrice }}</h2>
+    </div>
+    <div class="draw-animation" v-if="animationOn">
+      <lottie :options="lottieOptions" :height="650" :width="700" v-on:animCreated="handleAnimation"/>
+      <h3>and the lucky draw winner is ...</h3>
+    </div>
+    <div class="winner-prize" :style="{'order': giftId % 2 }" v-if="resultOn">
+      <div class="winner-prize-image">
+        <img class="prize-img" :src="imageUrl" />
+      </div>
+      <h1 v-html="giftName"></h1>
+      <h2>{{ giftPrice }}</h2>
+    </div>
+    <div class="winner" :style="{'order': (giftId + 1) % 2 }" v-if="resultOn">
+      <div class="winner-header">
+        <h1>Winners!</h1>
+      </div>
+      <div class="winner-list">
+        <p v-for="winner in winners" :key="winner.id" :class="winner.status">{{ winner.username }} - {{ winner.firstName }} {{ winner.lastName }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import lottie from "vue-lottie";
+import * as animationData from "@/assets/luckyDraw.json";
 export default {
+  components: {
+    lottie
+  },
+  data() {
+    return {
+      lottieOptions: {
+        animationData: animationData.default
+      },
+      animationOn: false,
+      resultOn: false
+    };
+  },
+  mounted() {
+    this.$store.watch(
+      () => {
+        return this.$store.state.luckyDraw.winners;
+      },
+      winners => {
+        if (winners.length > 0) {
+          this.animationOn = true;
+          this.resultOn = false;
+          setTimeout(() => {
+            this.animationOn = false;
+            this.resultOn = true;
+          }, 3500);
+        }
+      },
+      {
+        deep: true
+      }
+    );
+  },
+  methods: {
+    handleAnimation: function(anim) {
+      this.anim = anim;
+    }
+  },
   computed: {
     giftName() {
       return this.$store.state.luckyDraw.currentGift.name;
+    },
+    giftPrice() {
+      return this.$store.state.luckyDraw.currentGift.price;
+    },
+    giftId() {
+      return this.$store.state.luckyDraw.currentGift.id;
     },
     imageUrl() {
       return this.$store.state.luckyDraw.currentGift.imageUrl;
@@ -26,18 +93,112 @@ export default {
 </script>
 
 <style>
+h1 {
+  font-size: 40pt;
+  padding: 15px;
+  font-family: "TT Norms";
+  font-weight: 800;
+  font-style: normal;
+}
+h2 {
+  font-size: 30pt;
+  /*padding: 15px;*/
+  font-family: "TT Norms";
+  font-weight: bold;
+  font-style: normal;
+}
+h3 {
+  font-size: 30pt;
+  padding: 15px;
+  font-family: "TT Norms";
+  font-weight: 100;
+  font-style: italic;
+}
+.lucky_draw {
+  height: 100vh;
+  color: white;
+  background: #0033a0;
+  display: flex;
+}
+.gift-photo {
+  height: 100vh;
+  width: 50vw;
+  background: white;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+}
+.draw-animation {
+  height: 100vh;
+  width: 100vw;
+}
+.gift-detail {
+  height: 100vh;
+  width: 50vw;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+}
+.winner {
+  /*border-left: solid 1px white;*/
+  height: 100vh;
+  width: 50vw;
+  /*background: white;*/
+  font-family: "TT Norms";
+  font-weight: bold;
+  font-style: normal;
+  font-size: 30px;
+  line-height: 1.7em;
+}
+.winner-prize {
+  height: 100vh;
+  width: 50vw;
+  background: white;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+  text-align: center;
+}
+.winner-prize-image {
+  justify-content: center;
+  align-content: center;
+  text-align: center;
+}
+.winner-prize > img {
+  max-height: 50vh;
+}
+.winner-header {
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+  text-align: center;
+}
+.winner-list {
+  height: 60vh;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+  text-align: center;
+}
 .new {
-  color: lightgreen;
-  text-decoration: underline;
+  /*color: lightgreen;*/
+  /*text-decoration: underline;*/
 }
 .existing {
   color: grey;
 }
 .removed {
-  color: red;
-  text-decoration: line-through;
+  display: none;
 }
-img {
-  width: 80%;
+.prize-img {
+  max-width: 45vw;
+  max-height: 90vh;
 }
 </style>
