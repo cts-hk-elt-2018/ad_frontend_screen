22be48cc90f7994cc2041cf875d410f18d4ee75a
ckho <me@ckho.info>
2018-11-18 01:26:38 +0800

    Backdrop Page

---
+++ src/views/BackDrop.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="backdrop">
+    <img class="whole" src="../assets/backdrop2.png" />
+  </div>
+</template>
+
+<style lang="scss">
+.whole {
+  width: 100%;
+}
+body {
+  background: black;
+}
+</style>
