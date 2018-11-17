5e674b58fa7fe1232974d1950cae757d639e127c
ckho <me@ckho.info>
2018-11-18 01:28:52 +0800

    Lucky Draw Layout with animation

--- src/views/Award.vue
+++ src/views/Award.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an award page</h1>
+  <div class="award">
+    <h1>{{ award }}</h1>
+    <p>{{ awardee.firstName }} {{ awardee.lastName }} ({{ awardee.staffID }})</p>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    award() {
+      return this.$store.state.award.currentAward.name;
+    },
+    awardee() {
+      return this.$store.state.award.awardee;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.award {
+  color: white;
+}
+</style>
